df89dadf93caeb0ad84998c4f887fec317b47077
Roseland Andisi Ambuku <roselandambuku@gmail.com>
2024-06-11 18:15:37 +0300

    feat(rick-morty): adds rick and morty page

---
+++ app/rick-morty-api/page.tsx
@@ -0,0 +1,7 @@
+export default function RickMortyApi() {
+    return(
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        Rick & Morty Page
+    </main>
+    )
+}
