3eb4526603d6c013d605f422b50bd7ca8a5d6466
Roseland Andisi Ambuku <roselandambuku@gmail.com>
2024-06-11 18:16:25 +0300

    feat(unsplash): adds unsplash pages

---
+++ app/unsplash-api/page.tsx
@@ -0,0 +1,14 @@
+import { getAllPhotos, getOnePhoto, getRandomPhoto } from "@/lib/data/unsplash-data"
+
+export default async function UnsplashApi() {
+    // const data = await getAllPhotos()
+    // const data = await getOnePhoto(id)
+    // const data = await getRandomPhoto()
+    // console.log(data)
+    return(
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        Unsplash Page
+        
+    </main>
+    )
+}
